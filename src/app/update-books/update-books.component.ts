cdd357c83856c32a9a1f45aca088bcface313a58
ABAD81280 <abad81280@gmail.com>
2023-10-22 17:16:36 +0300

    3 commit

--- src/app/update-books/update-books.component.ts
+++ src/app/update-books/update-books.component.ts
@@ -11,6 +11,13 @@ import {Location} from '@angular/common';
 export class UpdateBooksComponent {
   allBooks!:Observable<any>
 constructor(private crud:CRUDbooksService,private ro :Router,private location:Location){this.getAllBook()}
+thcategory=[{category:"التنمية البشرية وتطوير الذات"},
+              {category:"الحرب والعلوم العسكرية"},
+              {category:"الحب والعلاقات الأسرية"},
+              {category:"العلوم الاسلامية"},
+              {category:"التسويق وإدارة الأعمال"},
+              {category:"مذكرات وسير ذاتية"},
+              {category:"روايات رعب"}]
 Admen()
   {
     const t = sessionStorage.getItem("UT")
@@ -41,6 +48,10 @@ setValeUpdate(url:string,name:string,description:string,writer:string,category:s
 updateBook()
 {
   this.crud.updateBooks(this.url,this.name,this.category,this.description,this.writer)
+}
+setTheCategory(category:string)
+{
+  this.category = category
 }
 //
 goBack()
